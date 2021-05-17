3cb2318cf656ce25c124c370f0b29178227fb3c2
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-05-17 17:38:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 func myFunc(w http.ResponseWriter, _ *http.Request){
 	io.WriteString(w, "deneme")
 }
+func myFunc2(w http.ResponseWriter, _ *http.Request){
+	io.WriteString(w, "deneme2")
+}
+func myFunc3(w http.ResponseWriter, _ *http.Request){
+	io.WriteString(w, "deneme3")
+}
 
 func main(){
-	http.ListenAndServe(":8080",http.HandlerFunc(myFunc))
+	http.Handle("/", http.HandlerFunc(myFunc2))
+	http.Handle("/", http.HandlerFunc(myFunc3))
+	http.ListenAndServe(":8080", http.HandlerFunc(myFunc))
 }
