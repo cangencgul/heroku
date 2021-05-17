31e5ddc0f17911c70a66327ee655e289d651cb36
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-05-17 15:45:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,4 +11,17 @@ func main() {
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
     fmt.Println(w, "fasafiso")
   })
+  
+  s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Print("There is a Error from http.ListenAndServe it is ", err)
+	}
 }
