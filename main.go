bf0e01435199265b2f061e6a3bf85065dd35313e
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-05-17 17:31:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
-import(
-  "fmt"
-  "net/http"
-  "golang.org/x/net/html"		
+import (
+	"io"
+	"net/http"
 )
 
-func main() {
-  
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
-  
-  s := &http.Server{
-		Addr:           ":8080",
-		Handler:        mux,
-		MaxHeaderBytes: 1 << 20,
-	}
+func myFunc(w http.ResponseWriter, _ *http.Request){
+	io.WriteString(w, "deneme")
+}
 
-	err := s.ListenAndServe()
-	if err != nil {
-		fmt.Print("There is a Error from http.ListenAndServe it is ", err)
-	}
+func main(){
+	http.ListenAndServe(":8080",http.HandlerFunc(myFunc))
 }
