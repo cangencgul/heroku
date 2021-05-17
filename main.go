6dd97e9ed2e0454bf5920f7a2197e78b9d4757f2
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-05-17 18:09:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"html/template"
 	"io"
 	"os"
 	"log"
 	"net/http"
 )
 
-func myFunc(w http.ResponseWriter, _ *http.Request){
-	io.WriteString(w, "deneme")
-}
 func myFunc2(w http.ResponseWriter, _ *http.Request){
+	t, _ := template.ParseFiles("main.html")
+	t.Execute(w, _)
 	io.WriteString(w, "deneme2")
 }
 func myFunc3(w http.ResponseWriter, _ *http.Request){
