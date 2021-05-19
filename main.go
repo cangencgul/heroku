4461b8e3b530c947fe62c803c4ece534c72712cf
cangencgul <mustafacangencgul@gmail.com>
2021-05-19 09:09:16 +0300

    css dosyasına erişemiyordum onunla ilgili bir düzenleme yaptım

--- main.go
+++ main.go
@@ -2,33 +2,34 @@ package main
 
 import (
 	"html/template"
-	"os"
 	"log"
 	"net/http"
+	"os"
 )
 
-func myFunc2(w http.ResponseWriter, _ *http.Request){
+func myFunc2(w http.ResponseWriter, _ *http.Request) {
 	t, _ := template.ParseFiles("main.html")
 	t.Execute(w, "fff")
 }
 
-func myFunc3(w http.ResponseWriter, _ *http.Request){
+func myFunc3(w http.ResponseWriter, _ *http.Request) {
 	t, _ := template.ParseFiles("map.html")
 	t.Execute(w, "fff")
 }
 
-func myFunc4(w http.ResponseWriter, _ *http.Request){
+func myFunc4(w http.ResponseWriter, _ *http.Request) {
 	t, _ := template.ParseFiles("option.html")
 	t.Execute(w, "fff")
 }
 
-func main(){
+func main() {
 	port := os.Getenv("PORT")
-    	if port == "" {
-        	log.Fatal("error")
-    	}
+	if port == "" {
+		log.Fatal("error")
+	}
 	http.Handle("/", http.HandlerFunc(myFunc2))
 	http.Handle("/3", http.HandlerFunc(myFunc3))
 	http.Handle("/option", http.HandlerFunc(myFunc4))
+	http.Handle("/heroku/assets/", http.StripPrefix("/heroku/assets/", http.FileServer(http.Dir("./heroku/assets"))))
 	http.ListenAndServe(":"+port, nil)
 }
